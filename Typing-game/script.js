5edfdc9a627b02357441cbd26026290c2d9e550d
Vimala S <vimala.sellappan@stud.sundsgarden.se>
2025-04-28 13:39:39 +0200

    Add Typing-game files after removing embedded Git repo

---
+++ Typing-game/script.js
@@ -0,0 +1,53 @@
+// Variables for the DOM elements
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endgameEl = document.getElementById("end-game-container");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const settingsBtn = document.getElementById("settings-btn");
+const difficultySelect = document.getElementById("difficulty");
+
+// Array
+const words = [
+  "dependent",
+  "dog",
+  "superficial",
+  "admit",
+  "juice",
+  "javascript",
+  "developer",
+  "airplane",
+  "great",
+  "fun",
+  "manipulate",
+  "cat",
+  "transition",
+  "school",
+  "computer",
+  "programming",
+  "drag",
+  "loving",
+  "north",
+];
+
+//Initializing word
+let randomWord;
+
+//Initializing score
+let score = 0;
+
+//Initializing time
+let time = 10;
+function addWordToDOM()
+ {
+  randomWord = words[Math.floor(Math.random() * words.length)];
+  word.innerHTML = randomWord;
+}
+function updateScore() 
+{
+  score++;
+  scoreEl.innerHTML = score;
+}
+addWordToDOM();
