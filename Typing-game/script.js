29b839c0055873b9e59f2cb19b8f06e42e0ea8dc
Vimala S <vimala.sellappan@stud.sundsgarden.se>
2025-05-01 22:25:31 +0200

    part 1 and part 2 completed as per given instructions

--- Typing-game/script.js
+++ Typing-game/script.js
@@ -40,6 +40,7 @@ let score = 0;
 
 //Initializing time
 let time = 10;
+let timer;
 function addWordToDOM()
  {
   randomWord = words[Math.floor(Math.random() * words.length)];
@@ -50,4 +51,40 @@ function updateScore()
   score++;
   scoreEl.innerHTML = score;
 }
+function updateTime()
+{
+  if (time > 0) 
+  {
+    time--; 
+    timeEl.innerHTML = `${time}s`; 
+  } 
+  else
+  {
+    gameOver(); 
+  }
+}
+function gameOver() 
+{
+
+  endgameEl.innerHTML = `<h1>Game Over!</h1>
+    <p>Your final score is: ${score}</p>
+    <button onclick="location.reload()">Restart Game</button>
+`;
+    endgameEl.style.display = "flex";
+    clearInterval(timer); 
+}
 addWordToDOM();
+timer = setInterval(updateTime, 1000); 
+text.addEventListener("input", () => {
+  const typedText = text.value;
+  if (typedText === randomWord)
+ {
+    addWordToDOM();         
+    updateScore();          
+    text.value = "";        
+    time += 5;              
+    updateTime();           
+  }
+});
+
+
